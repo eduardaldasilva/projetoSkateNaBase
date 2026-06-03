32785f8139db4769ea18c88e516f9daaaa4751f4
eduardaldasilva <contatoeduardadasilva@gmail.com>
2026-06-02 21:34:34 -0300

    Estruturando o banco

---
+++ database/skateNaBase.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE skateNaBase;
+\c skateNaBase;
+
+CREATE TYPE status_presenca AS ENUM ('Presente', 'Ausente');
+CREATE TYPE status AS ENUM ('Ativo', 'Inativo');
+
+
+CREATE TABLE alunos (
+    id_aluno SERIAL,
+    nome VARCHAR(150),
+    nome_responsavel VARCHAR(150),
+    status_aluno status,
+    data_nascimento date,
+    telefone VARCHAR(11),
+    cep VARCHAR(9),
+    logradouro VARCHAR(200),
+    numero VARCHAR(5),
+    bairro VARCHAR(100)
+);
+
+CREATE TABLE intrutores (
+    id_instrutor SERIAL,
+    nome VARCHAR(150),
+    status_instrutor status
+    telefone VARCHAR(11),
+    cep VARCHAR(9),
+    logradouro VARCHAR(200),
+    numero VARCHAR(5),
+    bairro VARCHAR(100)
+);
+
+CREATE TABLE frequencia (
+    id_presenca SERIAL,
+    data_presenca DATETIME,
+    presenca status_presenca
+);
+
+CREATE TABLE turma (
+    id_turma SERIAL,
+    data_aula DATETIME,
+    qtd_vagas VARCHAR(2)
+);
+
+ALTER TABLE alunos ADD CONSTRAINT pk_alunos PRIMARY KEY (id_aluno);
+ALTER TABLE frequencia ADD CONSTRAINT pk_presencas PRIMARY KEY (id_presenca);
+ALTER TABLE turma ADD CONSTRAINT pk_turma PRIMARY KEY (id_turma);
+
+ALTER TABLE frequencia ADD CONSTRAINT fk_presencas_aluno FOREIGN KEY (id_aluno) REFERENCES alunos (id_aluno) ON DELETE CASCADE;
+ALTER TABLE turma ADD CONSTRAINT fk_turma_instrutor FOREIGN KEY (id_instrutor) REFERENCES intrutores (id_instrutor) ON DELETE CASCADE;
