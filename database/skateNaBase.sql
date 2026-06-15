cfc4f069195b1eded0dea8be1cb5c4df41e08870
eduardaldasilva <contatoeduardadasilva@gmail.com>
2026-06-15 16:09:10 -0300

    Ajustes

--- database/skateNaBase.sql
+++ database/skateNaBase.sql
@@ -5,11 +5,11 @@ CREATE DATABASE "skateNaBase";
 CREATE TYPE status_frequencia AS ENUM ('Presente', 'Ausente');
 CREATE TYPE status AS ENUM ('Ativo', 'Inativo');
 
-create table usuarios (
-    id serial,
-    login varchar(150) unique not null,
-    senha varchar(255) not null,
-    role varchar(50) not null
+CREATE TABLE usuarios (
+    id SERIAL,
+    login VARCHAR(150) UNIQUE NOT NULL,
+    senha VARCHAR(10) NOT NULL,
+    role VARCHAR(14) NOT NULL
 );
 
 CREATE TABLE alunos (
@@ -19,46 +19,77 @@ CREATE TABLE alunos (
     nome_responsavel VARCHAR(150),
     status_aluno status,
     data_nascimento DATE,
-    telefone VARCHAR(11),
-    cep VARCHAR(9),
+    telefone VARCHAR(18),
+    cep VARCHAR(10),
     logradouro VARCHAR(200),
     numero VARCHAR(5),
-    bairro VARCHAR(100)
+    bairro VARCHAR(150)
 );
 
 CREATE TABLE instrutores (
     id_instrutor SERIAL,
     nome VARCHAR(150),
     status_instrutor status,
-    telefone VARCHAR(11),
-    cep VARCHAR(9),
+    telefone VARCHAR(18),
+    cep VARCHAR(10),
     logradouro VARCHAR(200),
     numero VARCHAR(5),
-    bairro VARCHAR(100)
+    bairro VARCHAR(150)
 );
 
-CREATE TABLE frequencia (
-    id_presenca SERIAL,
+CREATE TABLE frequencias (
+    id_presenca SERIAL,aluno
     id_aluno INT,
     data_registro TIMESTAMP,
     status_presenca status_frequencia
 );
 
-CREATE TABLE turma (
+CREATE TABLE turmas (
     id_turma SERIAL,
     data_aula VARCHAR(15),
     horario TIME,
     qtd_vagas INT,
     id_instrutor INT
+    nivel VARCHAR(14);
 );
+
+CREATE TABLE noticias (
+    id_noticia SERIAL,
+    titulo VARCHAR(250),
+    descricao TEXT,
+    data_criacao TIMESTAMP,
+    status_noticia status,
+    id_turma INT;
+);
+
+CREATE TABLE avaliacoes (
+    id_avaliacao SERIAL,
+    data_avaliacao DATE,
+    nivel VARCHAR(14),
+    id_aluno INT;
+);
+
+CREATE TABLE criterios_de_desempenho (
+    id_criterio SERIAL,
+    nome_criterio VARCHAR(100),
+    pontuacao INT,
+    id_avaliacao INT;
+);
+
 
 ALTER TABLE alunos ADD CONSTRAINT pk_alunos PRIMARY KEY (id_aluno);
 ALTER TABLE instrutores ADD CONSTRAINT pk_instrutores PRIMARY KEY (id_instrutor);
-ALTER TABLE frequencia ADD CONSTRAINT pk_presencas PRIMARY KEY (id_presenca);
-ALTER TABLE turma ADD CONSTRAINT pk_turma PRIMARY KEY (id_turma);
+ALTER TABLE frequencias ADD CONSTRAINT pk_presencas PRIMARY KEY (id_presenca);
+ALTER TABLE turmas ADD CONSTRAINT pk_turmas PRIMARY KEY (id_turma);
 ALTER TABLE usuarios ADD CONSTRAINT pk_usuarios PRIMARY KEY (id);
+ALTER TABLE noticias ADD CONSTRAINT pk_noticias PRIMARY KEY (id_noticia);
+ALTER TABLE avaliacoes ADD CONSTRAINT pk_avaliacoes PRIMARY KEY (id_avaliacao);
+ALTER TABLE criterios_de_desempenho ADD CONSTRAINT pk_criterio PRIMARY KEY (id_criterio);
 
-ALTER TABLE frequencia ADD CONSTRAINT fk_presencas_aluno FOREIGN KEY (id_aluno) REFERENCES alunos (id_aluno) ON DELETE CASCADE;
-ALTER TABLE turma ADD CONSTRAINT fk_turma_instrutor FOREIGN KEY (id_instrutor) REFERENCES instrutores (id_instrutor) ON DELETE CASCADE;
-ALTER TABLE alunos ADD CONSTRAINT fk_turma_aluno FOREIGN KEY (id_turma) REFERENCES turma (id_turma) ON DELETE CASCADE;
+ALTER TABLE frequencias ADD CONSTRAINT fk_presencas_aluno FOREIGN KEY (id_aluno) REFERENCES alunos (id_aluno) ON DELETE CASCADE;
+ALTER TABLE turmas ADD CONSTRAINT fk_turma_instrutor FOREIGN KEY (id_instrutor) REFERENCES instrutores (id_instrutor) ON DELETE CASCADE;
+ALTER TABLE alunos ADD CONSTRAINT fk_turma_aluno FOREIGN KEY (id_turma) REFERENCES turmas (id_turma) ON DELETE CASCADE;
+ALTER TABLE noticias ADD CONSTRAINT fk_noticia_turma FOREIGN KEY (id_turma) REFERENCES turmas (id_turma) ON DELETE CASCADE;
+ALTER TABLE avaliacoes ADD CONSTRAINT fk_avalicao_aluno FOREIGN KEY (id_aluno) REFERENCES alunos (id_aluno) ON DELETE CASCADE;
+ALTER TABLE criterios_de_desempenho ADD CONSTRAINT fk_avalicao_desempenho FOREIGN KEY (id_avaliacao) REFERENCES avaliacoes (id_avaliacao) ON DELETE CASCADE;
 
--- src/models/turmasModel.js
+++ src/models/turmasModel.js
@@ -1,29 +1,29 @@
 const db = require("../../config/pgpConnection");
 
 const obterTurmas = () => {
-  return db.any("select * from turma");
+  return db.any("select * from turmas");
 };
 
 const obterPorId = (id) => {
-  return db.oneOrNone("select * from turma where id_turma = $1", [id]);
+  return db.oneOrNone("select * from turmas where id_turma = $1", [id]);
 };
 
 const criar = (data_aula, horario, qtd_vagas, id_instrutor) => {
   return db.one(
-    "insert into turma(data_aula, horario, qtd_vagas, id_instrutor) values($1, $2, $3, $4) returning *",
+    "insert into turmas(data_aula, horario, qtd_vagas, id_instrutor) values($1, $2, $3, $4) returning *",
     [data_aula, horario, qtd_vagas, id_instrutor],
   );
 };
 
 const atualizar = (id, data_aula, horario, qtd_vagas, id_instrutor) => {
   return db.oneOrNone(
-    "update turma set data_aula = $1, horario = $2, qtd_vagas = $3, id_instrutor = $4 where id_turma = $5 returning *",
+    "update turmas set data_aula = $1, horario = $2, qtd_vagas = $3, id_instrutor = $4 where id_turma = $5 returning *",
     [data_aula, horario, qtd_vagas, id_instrutor, id],
   );
 };
 
 const excluir = (id) => {
-  return db.oneOrNone("delete from turma where id_turma = $1 returning *", [id]);
+  return db.oneOrNone("delete from turmas where id_turma = $1 returning *", [id]);
 };
 
 module.exports = { obterTurmas, obterPorId, criar, atualizar, excluir };
